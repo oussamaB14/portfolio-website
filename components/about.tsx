9ab1f3f7bfdaf20723539f63e0e48bf2abd099fb
oussamaB14 <88203283+oussamaB14@users.noreply.github.com>
2024-05-26 01:06:03 +0100

    update

--- components/about.tsx
+++ components/about.tsx
@@ -39,10 +39,11 @@ export default function About() {
       <p>
         <span className="italic">When I'm not coding</span>, I enjoy playing
         video games, watching movies. I also enjoy{" "}
-        <span className="font-medium">learning new things</span>. I am currently
+        <span className="font-medium">learning new things</span>.
+        {/* I am currently
         learning about{" "}
         <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
+        learning how to play the guitar. */}
       </p>
     </motion.section>
   );
